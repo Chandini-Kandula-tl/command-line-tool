8668d49c2e665a172eb522a865321a44e7ff8f29
Chandini-Kandula-tl <163842252+Chandini-Kandula-tl@users.noreply.github.com>
2024-03-19 14:14:07 +0530

    commandLineTool.js

--- commandLineTool.js
+++ commandLineTool.js
@@ -1,219 +1,324 @@
-const MODULES = { MathHandler: require('./MathHandler.js') }
-const { HELP_MSGS } = require('./constants.js')
-const History = require('./history.js')
-const { Logs, Log } = require('./logfile.js')
-//class of logfile
-
+const fs = require('fs');
+const path = require('path');
+const { HELP_MSGS } = require('./constants.js');
+const { Logs, Log } = require('./logfile.js');
+const History = require('../sprint-6_4_mar_to_08_mar_2024/history.js');
 
 class Constants {
-    static REGISTER = "register";
-    static UNREGISTER = "unregister";
-    static ACTIVE = "active";
-    static DEACTIVE = "deactive";
+    static CHANGEDIRECTORY = "cd";
+    static PRESENTWORKINGDIRECTORY = "pwd";
+    static LISTOFFILES = "ls";
+    static DISPLAYCONTENT = "cat";
+    static FIND = "find";
     static HISTORY = "history";
     static LOG = "log";
     static HELP = "--help";
-    static EXIT = "exit";
     static START = "--start";
-    static STOP = "--stop"
+    static STOP = "--stop";
+    static EXIT = "exit";
+    static A = "-a";
+    static FI = "-fi";
+    static FIG = "-fiG";
+    static DIR = "-dir";
+    static G = "-G"
+    static S = "s";
+    static E = "e";
+    static DOTS = "..";
+
 }
 
-const DEFAULT_CMD_CONDITIONS = {
-    [Constants.LOG]: { length: 3, formate: "<log> <command_name> <count>", isLengthNotConst: true },
-    [Constants.REGISTER]: { length: 3, formate: "<register> <command_name> <executor>", isLengthNotConst: false },
-    [Constants.UNREGISTER]: { length: 2, formate: "<unregister> <command_name>", isLengthNotConst: false },
-    [Constants.ACTIVE]: { length: 2, formate: "<active> <command_name>", isLengthNotConst: false },
-    [Constants.DEACTIVE]: { length: 2, formate: "<deactive> <command_name>", isLengthNotConst: false },
-    [Constants.HISTORY]: { length: 3, formate: "<history> <count>", isLengthNotConst: true },
-    [Constants.HELP]: { length: 3, formate: "<command_name> <--help>", isLengthNotConst: true }
-}
-
-
-class CommandTool {
+class FileViwer {
     constructor() {
-        this.registeredCommands = {};
-        this.validCommands = [Constants.REGISTER, Constants.UNREGISTER, Constants.ACTIVE, Constants.DEACTIVE, Constants.HISTORY, Constants.LOG, Constants.HELP];
-        this.history = new History()
-        this.logs = new Logs()
-        // this.logSerialNumber = 0;
-        this.commandStore = {
-            [Constants.LOG]: this.logHandler.bind(this),
-            [Constants.REGISTER]: this.register.bind(this),
-            [Constants.UNREGISTER]: this.unregister.bind(this),
-            [Constants.ACTIVE]: this.activate.bind(this),
-            [Constants.DEACTIVE]: this.deactivate.bind(this),
-            [Constants.HISTORY]: this.historyMethod.bind(this),
-            [Constants.HELP]: this.help.bind(this),
-        }
+        this.defaultFilePath = __dirname;
+        this.validCommands = [Constants.CHANGEDIRECTORY, Constants.PRESENTWORKINGDIRECTORY, Constants.LISTOFFILES,
+        Constants.DISPLAYCONTENT, Constants.FIND, Constants.LOG, Constants.HELP, Constants.HISTORY];
+        this.givenPath = this.defaultFilePath;
+        this.filesAndDirectories = { "Files": [], "Directories": [] };
+        this.history = new History();
+        this.logs = new Logs();
     }
 
-    handleCommand() {
+    handleInput() {
         while (true) {
-            let input = prompt("enter command : ");
-            this.history.storeHistory(input)
+            if (this.logs.isLogingStart === false) {
+                console.log("logs are in deactive state, please activate to store logs \nobserve the format : <log --start> <filepath>");
+            }
+            let input = prompt("enter the command : ");
+            this.history.storeHistory(input);
             let command = input.split(" ");
-            let mainCommand = command[(command.length) - 1] === Constants.HELP ? Constants.HELP : command[0];
             if (this.logs.isLogingStart) {
-                this.storeLogDeatails(command);
+                this.storeLogDetails(command);
             }
-            if (command[0] === Constants.HELP && command.length === 1) {
-                let keys = Object.keys(this.registeredCommands);
-                if (keys.length === 0) {
-                    console.log(HELP_MSGS);
-                }
-                else {
-                    let helpObj = {};
-                    for (let item of keys) {
-                        helpObj = { ...HELP_MSGS, [item]: { ...this.registeredCommands[item].object.help(["all"]) } }
-                    }
-                    console.log(helpObj)
-                }
+            if (command[0] === Constants.EXIT) {
+                break;
+            }
+            else if (command[command.length - 1] === Constants.HELP) {
+                this.helpMethod(command);
+            }
+            else if (command[0] === Constants.CHANGEDIRECTORY) {
+                this.changeDirectory(command);
+            }
+            else if (command[0] === Constants.PRESENTWORKINGDIRECTORY) {
+                this.presentWorkingDirectory();
+            }
+            else if (command[0] === Constants.LISTOFFILES) {
+                this.listOfFiles(command);
+            }
+            else if (command[0] === Constants.DISPLAYCONTENT) {
+                this.displayContent(command);
+            }
+            else if (command[0] === Constants.FIND) {
+                this.findFile(command);
+            }
+            else if (command[0] === Constants.HISTORY) {
+                this.historyMethod(command);
+            }
+            else if (command[0] === Constants.LOG) {
+                this.logHandler(command);
             }
             else {
-                if (mainCommand === Constants.EXIT) {
-                    break
+                console.log("invalid command");
+            }
+        }
+    }
+
+    changeDirectory(command) {
+        try {
+            let argument = command[1];
+            if (command.length === 1) {
+                console.log("<DIR> missing : please specify directory");
+                return;
+            }
+            else if (argument === Constants.DOTS) {
+                let temp = this.givenPath;
+                temp = temp.split("/");
+                temp.pop();
+                temp = temp.join("/");
+                this.givenPath = temp;
+                console.log(this.givenPath);
+            }
+            else if (argument) {
+                let temp = this.givenPath;
+                temp = temp.split("/");
+                temp.push(argument);
+                temp = temp.join("/");
+                if (fs.existsSync(temp)) {
+                    this.givenPath = temp;
+                    console.log(this.givenPath);
                 }
-                else if (!!this.commandStore[mainCommand]) {
-                    if (DEFAULT_CMD_CONDITIONS[mainCommand].isLengthNotConst || command.length === DEFAULT_CMD_CONDITIONS[mainCommand].length)
-                        this.commandStore?.[mainCommand](command);
+                else {
+                    console.log("invalid directory");
+                }
+            }
+        }
+        catch (err) {
+            console.log("invalid command");
+        }
+    }
+
+    presentWorkingDirectory(command) {
+        console.log(this.givenPath);
+    }
+
+    listOfFiles(command) {
+        try {
+            let argument = command[1];
+            if (argument === undefined) {
+                let fileNames = fs.readdirSync(this.givenPath);
+                console.log("\ncurrent directory filenames : ");
+                fileNames.forEach(file => {
+                    console.log(file);
+                });
+            }
+            else if (argument === Constants.A) {
+                let fileNames = fs.readdirSync(this.givenPath);
+                const hiddenFiles = fileNames.filter(file => file.startsWith("."));
+                console.log(hiddenFiles);
+            }
+            else if (argument === Constants.G) {
+                console.log(this.filesAndDirectories);
+            }
+            else if (argument === Constants.FI) {
+                this.seperateFilesDirectories();
+                console.log(this.filesAndDirectories["Files"]);
+
+            }
+            else if (argument === Constants.DIR) {
+                console.log(this.filesAndDirectories["Directories"]);
+            }
+            else if (argument === Constants.FIG) {
+                let groupOfFiles = {};
+                let directoryFiles = fs.readdirSync(this.givenPath);
+                directoryFiles.forEach(file => {
+                    let fileArr = file.split(".");
+                    if (fileArr[1] in groupOfFiles) {
+                        let temp = fileArr.join(".");
+                        groupOfFiles[fileArr[1]].push(temp);
+                    }
                     else {
-                        console.log("command or number of input commands are wrong. Please observe the format : " + DEFAULT_CMD_CONDITIONS[mainCommand].formate);
+                        groupOfFiles[fileArr[1]] = [];
+                        let temp = fileArr.join(".");
+                        groupOfFiles[fileArr[1]].push(temp);
                     }
+                })
+                console.log(groupOfFiles);
+            }
+            else {
+                console.log("invalid command");
+            }
+        }
+        catch (err) {
+            console.log("invalid command or error occured");
+        }
+    }
+
+    displayContent(command) {
+        try {
+        if (command.length === 1) {
+            console.log("please specify fileName");
+        }
+        else {
+            if (command.length < 3 && command[command.length - 1] !== typeof Number) {
+                this.catOperation(command)
+            }
+            else if(command.length === 3)
+            {
+                this.catOperation(command);
+            }
+        }
+        }
+        catch (err) {
+            console.log("invalid command");
+        }
+    }
+
+    catOperation(command) {
+        try{
+        let fileName = command[1];
+        let argument = command[2] ?? 5;
+        let relativePath = fileName;
+        let absolutePath = path.resolve(relativePath);
+        let check = absolutePath.split(".");
+        if (fs.existsSync(absolutePath)) {
+            let poppedElement = check.pop();
+            if (poppedElement !== "txt") {
+                console.log(".txt extension files only supported");
+                return;
+            }
+            else {
+                let fileContent = fs.readFileSync(absolutePath, 'utf8');
+                const lines = fileContent.split('\n').slice(0, Number(argument));
+                console.log(lines.join('\n'));
+            }
+        }
+        else
+        {
+            console.log("file not found");
+        }
+    }
+    catch(err)
+    {
+        console.log("invalid command or error occured");
+    }
+    }
+
+    findFile(command) {
+        try {
+            let name = command[1];
+            let argument = command[2] ?? "c";
+            if (command.length === 1) {
+                console.log("<NAME> missing - Please specify name to search");
+                return;
+            }
+            else {
+                if (argument === Constants.S) {
+                    let fileNames = this.getFileNames();
+                    let files = fileNames.filter(file => file.startsWith(name));
+                    console.log(files);
+                }
+                else if (argument === Constants.E) {
+                    let fileNames = this.getFileNames();
+                    let files = fileNames.filter(file => file.endsWith(name));
+                    console.log(files);
+                }
+                else if (argument) {
+                    let fileNames = this.getFileNames();
+                    let files = fileNames.filter(file => file.includes(name));
+                    console.log(files);
                 }
                 else {
-                    this.handleOtherCommands(command)
+                    console.log("invalid command");
                 }
+
             }
         }
-    }
-    handleOtherCommands(command) {
-        // command = command.split(" ");
-        if (command[0] in this.registeredCommands) {
-            if (this.registeredCommands[command[0]].isActive) {
-                let access = this.registeredCommands[command[0]]["object"];
-                access.handleInput(command);
-            }
-            else {
-                console.log("command not activated");
-            }
-        }
-        else {
-            console.log("command not registered");
+        catch (err) {
+            console.log("invalid command");
         }
     }
 
-    logHandler(command) {
-        // command = command.split(" ");
-        if (command[1] === Constants.START) {
-            this.logs.isLogingStart = true;
-            // this.logs.logCheck = false;
-            // this.storeLogDeatails(command);
-            this.logs.handleLog(command)
+    getFileNames() {
+        let directoryFiles = fs.readdirSync(this.givenPath);
+        let fileNames = [];
+        for (let file of directoryFiles) {
+            let temp = file.split(".");
+            fileNames.push(temp[0])
         }
-        else if (this.logs.isLogingStart) {
-            // this.storeLogDeatails(command);
-            this.logs.handleLog(command)
-        }
-        else {
-            console.log("log is not yet started");
-        }
+        return fileNames;
     }
 
-    help(command) {
-        // command = command.split(" ");
-        if (this.validCommands.includes(command[0])) {
+    seperateFilesDirectories() {
+        for (let key in this.filesAndDirectories) {
+            this.filesAndDirectories[key] = [];
+        }
+        let fileNames = fs.readdirSync(this.givenPath);
+        fileNames.forEach(file => {
+            const filePath = path.join(this.givenPath, file);
+            const fileStats = fs.statSync(filePath);
+            if (fileStats.isFile()) {
+                this.filesAndDirectories["Files"].push(file);
+            }
+            else if (fileStats.isDirectory()) {
+                this.filesAndDirectories["Directories"].push(file);
+            }
+        })
+    }
+
+    helpMethod(command) {
+        if (command.length === 1) {
+            console.log(HELP_MSGS);
+        }
+        else if (command.length === 2) {
             console.log(HELP_MSGS[command[0]]);
-        }
-        else if (!!this.registeredCommands?.[command[0]]) {
-            this.registeredCommands[command[0]]["object"].handleInput(command);
-        }
-        else console.log("module is not present in register");
-    }
-
-    register(command) {
-        // command = command.split(" ");
-        let module = MODULES?.[command[2]]
-        if (!!module) {
-            const baseClass = require('./baseClass.js');
-            let object = new module();
-            const isModulePresent = !!Object.values(this.registeredCommands).find(value => value.moduleName === command[2])
-            if (command[1] in this.registeredCommands || isModulePresent) {
-                console.log('command or module already registered');
-            }
-            else if (object instanceof baseClass) {
-                this.registeredCommands[command[1]] = { object, isActive: true, moduleName: command[2] };
-                console.log("command registered");
-                if (this.logs.isLogingStart === false) {
-                    console.log("logs are in deactive state, please activate to store logs \nobserve the format : <log --start> <filepath>");
-                }
-            }
-        }
-        else {
-            if (!module)
-                console.log(`Module not found,Please use any of this ${Object.keys(MODULES)}`);
-            else {
-                console.log("invalid Command");
-                console.log("observe the format : " + DEFAULT_CMD_CONDITIONS[mainCommand].formate);
-            }
-        };
-    }
-    unregister(command) {
-        // command = command.split(" ");
-        // console.log(command);
-        if (command[1] in this.registeredCommands) {
-            delete this.registeredCommands[command[1]];
-            console.log("command unregistered");
-        }
-        else {
-            console.log("invalid Command");
-            console.log("observe the format : " + DEFAULT_CMD_CONDITIONS[mainCommand].formate);
-        }
-    }
-    activate(command) {
-        // command = command.split(" ");
-        // console.log(command[1])
-        if (command[1] in this.registeredCommands) {
-            if (this.registeredCommands[command[1]].isActive) {
-                console.log("command already activated");
-            } else {
-                this.registeredCommands[command[1]].isActive = true;
-                console.log("command activated");
-            }
-        }
-        else {
-            console.log("command not registered");
-        }
-    }
-    deactivate(command) {
-        // command = command.split(" ");
-        if (command[1] in this.registeredCommands) {
-            if (!this.registeredCommands[command[1]].isActive) {
-                console.log("command already deactivated");
-            } else {
-                this.registeredCommands[command[1]].isActive = false;
-                console.log("command deactivated");
-            }
-        }
-        else {
-            console.log("command not registered");
         }
     }
     historyMethod(command) {
-        // command = command.split(" ");
         this.history.handleInput(command);
     }
 
-    storeLogDeatails(command) {
-        // let log = new Log({ logSerial: this.logs.totalLogs.length + 1, timeStamp: Date.now(), logCommand: command }); // object for logclass //object creation3
+    storeLogDetails(command) {
         command = command.join(" ");
         let log = new Log({ logSerial: this.logs.logSerialNumber, timeStamp: Date.now(), logCommand: command });
         this.logs.storeLog(log);
     }
 
+    logHandler(command) {
+        if (command[1] === Constants.START) {
+            this.logs.isLogingStart = true;
+            this.logs.handleLog(command);
+        }
+        else if (this.logs.isLogingStart) {
+            this.logs.handleLog(command);
+        }
+        else {
+            console.log("log is not yet started");
+        }
+    }
 }
-module.exports = CommandTool;
-// let prompt = promptsync();
+
 const prompt = require("prompt-sync")();
-const commandTool = new CommandTool();
-commandTool.handleCommand();
+const fileViwer = new FileViwer();
+fileViwer.handleInput();
+
+
 
